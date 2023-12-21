1178ad3a57033031d7789d53e8e05b004c5d0b1a
GwangjinJeong1 <dbsusqn12@gmail.com>
2023-12-21 20:02:13 +0900

    apply design system

--- lib/utils/text_theme.dart
+++ lib/utils/text_theme.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'color_scheme.dart';
+
 const TextStyle hm21 =
-    TextStyle(fontFamily: 'Pretendard-Medium', fontSize: 21, height: 25.1 / 21);
+    TextStyle(fontFamily: 'Pretendard-Medium', fontSize: 21, height: 25.1 / 21, color: AppColor.neutral);
 
 const TextStyle hb24 =
-    TextStyle(fontFamily: 'Pretendard-Bold', fontSize: 24, height: 28.6 / 24);
+    TextStyle(fontFamily: 'Pretendard-Bold', fontSize: 24, height: 28.6 / 24, color: AppColor.neutral);
 
 const TextStyle hb28 =
-    TextStyle(fontFamily: 'Pretendard-Bold', fontSize: 28, height: 33.4 / 28);
+    TextStyle(fontFamily: 'Pretendard-Bold', fontSize: 28, height: 33.4 / 28, color: AppColor.neutral);
 
 const TextStyle hs21 =
-    TextStyle(fontFamily: 'Pretendard-SemiBold', fontSize: 21, height: 32 / 21);
+    TextStyle(fontFamily: 'Pretendard-SemiBold', fontSize: 21, height: 32 / 21, color: AppColor.neutral);
 
 const TextStyle bs17 = TextStyle(
-    fontFamily: 'Pretendard-SemiBold', fontSize: 17, height: 20.3 / 17);
+    fontFamily: 'Pretendard-SemiBold', fontSize: 17, height: 20.3 / 17, color: AppColor.text);
 
 const TextStyle bs15 = TextStyle(
-    fontFamily: 'Pretendard-SemiBold', fontSize: 15, height: 17.9 / 15);
+    fontFamily: 'Pretendard-SemiBold', fontSize: 15, height: 17.9 / 15, color: AppColor.text);
 
 const TextStyle bm17 =
-    TextStyle(fontFamily: 'Pretendard-Medium', fontSize: 17, height: 20.3 / 17);
+    TextStyle(fontFamily: 'Pretendard-Medium', fontSize: 17, height: 20.3 / 17, color: AppColor.text);
 
 const TextStyle bm15 =
-    TextStyle(fontFamily: 'Pretendard-Medium', fontSize: 15, height: 26 / 15);
+    TextStyle(fontFamily: 'Pretendard-Medium', fontSize: 15, height: 26 / 15, color: AppColor.text);
 
 const TextStyle br15 = TextStyle(
-    fontFamily: 'Pretendard-Regular', fontSize: 15, height: 17.9 / 15);
+    fontFamily: 'Pretendard-Regular', fontSize: 15, height: 17.9 / 15, color: AppColor.text);
 
 const TextStyle sm13 =
-    TextStyle(fontFamily: 'Pretendard-Medium', fontSize: 13, height: 24 / 13);
+    TextStyle(fontFamily: 'Pretendard-Medium', fontSize: 13, height: 24 / 13, color: AppColor.text);
