dc17e6fb9f68ca86382b108553e52b6408023ba2
GwangjinJeong1 <dbsusqn12@gmail.com>
2023-12-21 16:32:50 +0900

    edit calendar.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'diary.dart';
 import 'drawer/calendar.dart';
 import 'drawer/wordcloud.dart';
 import 'home.dart';
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
           '/': (BuildContext context) => const HomePage(),
           '/wordcloud': (BuildContext context) => const WordCloudPage(),
           '/calendar': (BuildContext context) => const CalendarPage(),
+          '/diary': (BuildContext context) => const DiaryPage()
         },
         // theme: ThemeData.light(useMaterial3: true),
       );
